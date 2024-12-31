d95f75dd67484a4e4498a9969a2ae5270f27afcb
Rahul Singh <myacc2580@gmail.com>
2024-12-31 15:22:36 +0530

    added loops file

--- JsPractice/3loops.js
+++ JsPractice/3loops.js
@@ -41,3 +41,44 @@ do{
     console.log(h);
     h++;
 }while(h<2);
+
+//break
+// 1 to 100: print hi when you see the multiplication of 5 and break.
+console.log('---Break---');
+let num = 1;
+while(num < 100){
+    if(num % 5 == 0){
+        console.log("Hi");
+        break;
+    }
+    console.log(num);
+    num++;
+}
+
+//launch browser using break
+console.log('---Launch---');
+let Browsers = ["chrome", "firefox", "edge", "IE", "opera"];
+for(const e of Browsers){
+    if(e == "edge"){
+        console.log("launching edge...");
+        break;
+    }
+}
+
+// for...in loop on object
+console.log('---for...in---');
+const user = {
+    name: "rahul singh",
+    age: 27,
+    city: "Mainpuri"
+}
+for(const key in user){
+    console.log(key +" : "+ user[key]); //user[name]
+}
+
+console.log('---for...in---for indexes.');
+const br = ["chrome", "firefox", "edge", "IE", "opera"];
+for(const ele in br){
+    console.log(ele +": "+ br[ele]);
+    // console.log(br[ele]);  // to print the element on ele index
+}
