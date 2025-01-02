910fb332f6b60570442231b06bfce91a59029134
Rahul Singh <rahul.singh07@nagarro.com>
2025-01-03 01:50:28 +0530

    added 4 more types of functions to complete 9 types

--- JsPractice/7functions.js
+++ JsPractice/7functions.js
@@ -33,3 +33,65 @@ console.log(sub);
     console.log("Server is up and running at port 9090.");
 })
 ();
+
+//6. Generator Function:
+// function* Yield keyword
+function* generateNumberSequence(){
+    yield 1;
+    yield 6;
+    yield 3;
+    yield 8;
+}
+
+const generator = generateNumberSequence();
+console.log(generator.next().value);
+console.log(generator.next().value);
+console.log(generator.next().value);
+console.log(generator.next().value);
+
+// 7. Annonymous function: no specific function name
+let numbers = [1,2,3,4,5];
+const Numbersquare = numbers.map(function(e){
+    return e * e;
+});
+console.log(Numbersquare);
+
+// OR
+
+const Nsquare = numbers.map((e) => {
+    return e * e;
+});
+console.log(Nsquare);
+
+// 8. Recursive function: a function that calls itself.
+function factorial(n){
+    if(n === 0 || n ===1){
+        return 1;
+    }
+    else{
+        return n * factorial(n-1);
+    }
+}
+
+console.log(factorial(4));
+console.log(factorial(5));
+
+
+// Higher-Order function
+function add(s, t){
+    return s + t;
+}
+
+function multi(s, t){
+    return s * t;
+}
+
+function operator(funcName, s, t){
+    return funcName(s, t);
+}
+
+const s = operator(multi, 8, 5);
+console.log(s);
+
+const t = operator(add, 8, 5);
+console.log(t);
