d795b864ce5ddb3befa3477dc6ab701f6deac961
Rahul Singh <rahul.singh07@nagarro.com>
2025-01-04 23:24:30 +0530

    added typeOf use case

---
+++ JsPractice/10typeOfConcept.js
@@ -0,0 +1,32 @@
+// typeOf operator in java script to find the data type.
+let x = 25;
+console.log(typeof x);
+
+x = "check type";
+console.log(typeof x);
+
+x = {
+    name: "rahul"
+}
+console.log(typeof x);
+
+let flag = false;
+console.log(typeof flag);
+
+let type = typeof flag;
+console.log(typeof type);
+
+function print(){
+    console.log("Hello JS");
+}
+console.log("print: "+typeof print);
+
+const arr = [1,2,3,4,5];
+console.log(typeof arr);
+
+const j = null;
+console.log(typeof j);
+
+let y;
+console.log(typeof y);
+
